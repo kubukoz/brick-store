679e0ff81e07e31d5fd366b0cb14bded45e483b9
Jakub Kozłowski <kubukoz@gmail.com>
2019-06-05 21:19:14 -0600

    WIP test

--- src/test/scala/org/typelevel/brickstore/OrderServiceStreamingTests.scala
+++ src/test/scala/org/typelevel/brickstore/OrderServiceStreamingTests.scala
@@ -8,15 +8,17 @@ import cats.effect.implicits._
 import cats.effect.{Concurrent, ContextShift, IO, Timer}
 import cats.implicits._
 import fs2.Stream
+import fs2.concurrent.Queue
 import org.scalatest.compatible.Assertion
 import org.scalatest.{Matchers, WordSpec}
-import org.typelevel.brickstore.orders.{OrderService, OrderServiceImpl, OrderWithLines}
+import org.typelevel.brickstore.app.util.Now
+import org.typelevel.brickstore.orders.dto.OrderSummary
+import org.typelevel.brickstore.orders.{BrickOrder, OrderId, OrderService, OrderServiceImpl, OrderWithLines}
+import org.typelevel.brickstore.users.UserId
 
 import scala.concurrent.ExecutionContext.global
 
 class OrderServiceStreamingTests extends WordSpec with Matchers {
-
-  import org.typelevel.brickstore.app.util.Now.implicits._
 
   implicit val contextShift: ContextShift[IO] = IO.contextShift(global)
   implicit val timer: Timer[IO]               = IO.timer(global)
@@ -28,6 +30,7 @@ class OrderServiceStreamingTests extends WordSpec with Matchers {
 
   "streamAll" should {
     "be empty if both components are empty" in {
+      import org.typelevel.brickstore.app.util.Now.implicits._
 
       val test: IO[Assertion] = Deferred[IO, Unit].flatMap { finishLatch =>
         val orderService: OrderService[IO] = new OrderServiceImpl[IO, IO](
@@ -37,11 +40,44 @@ class OrderServiceStreamingTests extends WordSpec with Matchers {
           },
           new BricksRepositoryStub[IO],
           _ => IO.unit,
-          Stream.eval(finishLatch.complete(())).drain
+          Stream.eval_(finishLatch.complete(()))
         )
 
         collectToListUntilFinish(orderService.streamAll, finishLatch).map {
           _ shouldBe Chain.empty
+        }
+      }
+
+      test.unsafeRunSync()
+    }
+
+    "find elements that are produced to a queue after consuming the first streams begins" in {
+      implicit val now: Now[IO] = Now.liftF(IO.pure(Instant.EPOCH))
+
+      val sampleOrder  = OrderSummary(OrderId(1L), UserId(1L), 0, Instant.EPOCH)
+      val sampleOrderL = OrderWithLines(BrickOrder(OrderId(1L), UserId(1L), Instant.EPOCH), List())
+
+      val test: IO[Assertion] = Deferred[IO, Unit].flatMap { finishLatch =>
+        Deferred[IO, Unit].flatMap { startedConsumingExisting =>
+          Queue.bounded[IO, OrderSummary](100).flatMap { q =>
+            val existing = Stream.eval_(startedConsumingExisting.complete(())) ++ Stream
+              .emit(sampleOrderL)
+              .combineN(10)
+
+            val orderService: OrderService[IO] = new OrderServiceImpl[IO, IO](
+              new CartServiceStub[IO],
+              new OrderRepositoryStub[IO] {
+                override def streamExisting(before: Instant): Stream[IO, OrderWithLines] = existing
+              },
+              new BricksRepositoryStub[IO],
+              q.enqueue1,
+              Stream.eval_(startedConsumingExisting.get) ++ q.dequeue ++ Stream.eval_(finishLatch.complete(()))
+            )
+
+            collectToListUntilFinish(orderService.streamAll, finishLatch).map {
+              _ shouldBe Chain(sampleOrder).combineN(10)
+            }
+          }
         }
       }
 
